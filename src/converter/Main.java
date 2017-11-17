b4e18a3d88f83d854ae8972c9bbae0f1f7cf5541
nikita <evne.yankovnikita@gmail.com>
2017-11-17 09:15:34 +0200

    Whyyyyyyy package name is still 'sample'

--- src/converter/Main.java
+++ src/converter/Main.java
@@ -1,22 +1,16 @@
-package sample;
+package converter;
 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.GridPane;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.*;
